354ba90d1adc45e5b97cf375948b1ec6ffdcaeb8
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-10-23 16:35:40 +0300

    task 0 fix 3

--- markdown2html.py
+++ markdown2html.py
@@ -19,14 +19,30 @@ import sys
 import os
 import markdown
 
+def convert_md_to_html(md_file, html_file):
+    """
+    Converts a Markdown file to an HTML file.
+
+    Parameters:
+    - md_file (str): The path to the input Markdown file.
+    - html_file (str): The path to the output HTML file.
+
+    Returns:
+    - None
+    """
+    with open(md_file, 'r') as f:
+        md_content = f.read()
+        html_content = markdown.markdown(md_content)
+
+    with open(html_file, 'w') as f:
+        f.write(html_content)
+
 if __name__ == "__main__":
-    # check if the correct number of arguments have been passed
+    # Check if the correct number of arguments is passed
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
 
-    # Assign arguments to file names
     md_file = sys.argv[1]
     html_file = sys.argv[2]
 
@@ -36,13 +52,8 @@ if __name__ == "__main__":
         exit(1)
 
     # Convert Markdown to HTML
-    with open(md_file, 'r') as f:
-        md_content = f.read()
-        html_content = markdown.markdown(md_content)
-
-    # Write the HTML content to the output file
-    with open(html_file, 'w') as f:
-        f.write(html_content)
+    convert_md_to_html(md_file, html_file)
 
     # Successful completion
     exit(0)
+
